86760c8030672b2aaf073a5153575191bb21e424
Tim <voodookiidoo@yandex.ru>
2021-12-08 22:06:08 +0300

    My first commit

---
+++ cmake-build-debug/CMakeFiles/booba.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/booba.dir/main.cpp.o"
+  "booba"
+  "booba.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/booba.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "vector"
+
+using namespace std;
+typedef vector<int> vint;
+
+int main() {
+	vint nums = {0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0};
+	int max_dist = 0;
+	int max_ind = 0;
+	int left, right, zero;
+	for (zero = 0; zero < nums.size(); zero++) {
+		if (nums[zero] != 0) continue;
+		for (left = zero, right = zero;;) {
+			if (nums[left] != 1 & left >= 0) left--;
+			if (nums[right] != 1 & right <= nums.size()) right++;
+			if ((nums[left] == 1 || left < 0) && (nums[right] == 1 || right > nums.size())) break;
+		}
+		if (left >= 0 && right <= nums.size()) {
+			if (min(right - zero, zero - left) > max_dist) {
+				max_ind = zero;
+				max_dist = (min(right - zero, zero - left));
+			}
+		} else {
+			if (left < 0 && right - zero > max_dist) {
+				max_ind = zero;
+				max_dist = right - zero;
+			} else if (right > nums.size() && zero - left > max_dist) {
+				max_ind = zero;
+				max_dist = zero - left;
+			}
+		}
+	}
+	cout << max_ind;
+}
