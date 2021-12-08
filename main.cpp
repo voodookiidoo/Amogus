1f468ffecd06399cc6b75206f60ec23eb2e282fd
Tim <voodookiidoo@yandex.ru>
2021-12-09 00:30:04 +0300

    My second commit

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main() {
 	vint nums = {0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 0};
 	int max_dist = 0;
 	int max_ind = 0;
+	cout << '1';
 	int left, right, zero;
 	for (zero = 0; zero < nums.size(); zero++) {
 		if (nums[zero] != 0) continue;
@@ -18,6 +19,7 @@ int main() {
 		}
 		if (left >= 0 && right <= nums.size()) {
 			if (min(right - zero, zero - left) > max_dist) {
+
 				max_ind = zero;
 				max_dist = (min(right - zero, zero - left));
 			}
